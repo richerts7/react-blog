1f1b063e77faea72f68407102d8a9b34b95bd06a
richerts7 <63609821+richerts7@users.noreply.github.com>
2023-02-15 16:32:58 +0530

    react-blog

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './Pages/Home';
+import Bollywood from './Pages/Bollywood';
+import Technology from './Pages/Technology';
+import Hollywood from './Pages/Hollywood';
+import Fitness from './Pages/Fitness';
+import Food from './Pages/Food';
+import NextPage from './Pages/NextPage';
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <BrowserRouter>
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/bollywood" element={<Bollywood />} />
+          <Route path="/technology" element={<Technology />} />
+          <Route path="/hollywood" element={<Hollywood />} />
+          <Route path="/fitness" element={<Fitness />} />
+          <Route path="/food" element={<Food />} />
+          <Route path="/nextPage" element={<NextPage />} />
+        </Routes>
+
+      </BrowserRouter>
+
+
+    </>
   );
 }
 
